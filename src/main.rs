70a1b353c39088ee67ec7e25b4f89acbace75b62
Conor <conorjmoran4@gmail.com>
2023-08-13 22:54:17 +0100

    added functionality to identify processes with visible windows

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+extern crate winapi;
 use sysinfo::{ProcessExt, System, SystemExt};
+use std::ptr;
+use winapi::shared::windef::HWND;
+use winapi::um::winuser::{EnumWindows, GetWindowTextW, IsWindowVisible};
 
 fn main() {
     let mut s = System::new_all();
@@ -15,5 +19,26 @@ fn main() {
     for (pid, name, memory_bytes) in p.iter() {
         let memory_mb = (*memory_bytes as f64) / (1024.0 * 1024.0);
         println!("PID: {}, Name: {}, Memory: {:.2} MB", pid, name, memory_mb);
+    }
+
+    find_visible_windows();
+}
+
+fn find_visible_windows() {
+    unsafe extern "system" fn enum_windows_callback(hwnd: HWND, _l_param: isize) -> winapi::ctypes::c_int {
+        let mut buffer = [0u16; 256];
+        GetWindowTextW(hwnd, buffer.as_mut_ptr(), buffer.len() as i32);
+    
+        let window_title = String::from_utf16_lossy(&buffer);
+    
+        if !window_title.trim().is_empty() && IsWindowVisible(hwnd) != 0 {
+            println!("Visible window: {}", window_title.trim());
+        }
+    
+        1 // Continue enumeration
+    }
+
+    unsafe {
+        EnumWindows(Some(enum_windows_callback), 0);
     }
 }
