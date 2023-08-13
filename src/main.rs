205ea6836eb7ce8e4bf10ae05589ccd5349063ad
Conor <conorjmoran4@gmail.com>
2023-08-13 17:35:09 +0100

    Return the PID as well as memory usage in MB

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use sysinfo::{ProcessExt, System, SystemExt};
+
 fn main() {
     let mut s = System::new_all();
     s.refresh_all();
     
-    let mut p: Vec<(String, u64)> = Vec::new();
+    let mut p: Vec<(String, String, u64)> = Vec::new();
 
-    for (_pid, process) in s.processes() {
-        p.push((process.name().to_owned(), process.memory()));
+    for (pid, process) in s.processes() {
+        p.push((pid.to_string(), process.name().to_owned(), process.memory()));
     }
 
-    p.sort_by(|a, b| b.1.cmp(&a.1));
+    p.sort_by(|a, b| b.2.cmp(&a.2));
 
-    for (name, memory) in p.iter() {
-        println!("Name: {}, Memory: {}", name, memory);
+    for (pid, name, memory_bytes) in p.iter() {
+        let memory_mb = (*memory_bytes as f64) / (1024.0 * 1024.0);
+        println!("PID: {}, Name: {}, Memory: {:.2} MB", pid, name, memory_mb);
     }
 }
