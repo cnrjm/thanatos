85f0f73defdf5edc4c1fbfc284f10eba30cd36a5
Conor <conorjmoran4@gmail.com>
2023-09-11 17:02:01 +0100

    reformatted output

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,41 @@
-extern crate winapi;
 use sysinfo::{ProcessExt, System, SystemExt};
-use std::ptr;
-use winapi::shared::windef::HWND;
-use winapi::um::winuser::{EnumWindows, GetWindowTextW, IsWindowVisible, PostMessageW, WM_CLOSE};
 use std::collections::HashMap;
 use std::process::Command;
-use inputbot::{KeySequence, KeybdKey::*, MouseButton::*};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
-    Numrow9Key.bind(||main());
     let mut s = System::new_all();
     s.refresh_all();
-    
-    let mut process_memory_map: HashMap<String, u64> = HashMap::new();
-
-    for (_pid, process) in s.processes() {
-        let process_name = process.name().to_owned();
-        let memory_usage = process.memory();
-
-        let entry = process_memory_map.entry(process_name).or_insert(0);
-        *entry += memory_usage;
-    }
-
-    let mut p: Vec<(String, u64)> = process_memory_map.into_iter().collect();
-    p.sort_by(|a, b| b.1.cmp(&a.1));
-
-    for (index, (name, memory_bytes)) in p.iter().enumerate() {
-        let memory_mb = (*memory_bytes as f64) / (1024.0 * 1024.0);
-        println!("{}. Name: {}, Memory: {:.2} MB", index + 1, name, memory_mb);
-    }
-
-    find_visible_windows();
 
     loop {
+        let mut process_memory_map: HashMap<String, u64> = HashMap::new();
+
+        for (_pid, process) in s.processes() {
+            let process_name = process.name().to_owned();
+            let memory_usage = process.memory();
+
+            let entry = process_memory_map.entry(process_name).or_insert(0);
+            *entry += memory_usage;
+        }
+
+        let mut p: Vec<(String, u64)> = process_memory_map.into_iter().collect();
+        p.sort_by(|a, b| b.1.cmp(&a.1));
+
+        println!(
+            "\x1B[1m\x1B[4m{:<4} {:<30} {:<10}\x1B[0m",
+            "ID", "Name", "Memory (MB)"
+        );
+
+        for (index, (name, memory_bytes)) in p.iter().enumerate() {
+            let memory_mb = (*memory_bytes as f64) / (1024.0 * 1024.0);
+            println!("{:<4} {:<30} {:.2} MB", index + 1, name, memory_mb);
+        }
+
         let mut input = String::new();
-        std::io::stdin().read_line(&mut input).expect("Failed to read line");
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
         let input = input.trim().to_lowercase();
 
         if input.starts_with("kill ") {
@@ -43,6 +44,9 @@ fn main() {
                 Ok(index) if index > 0 && index <= p.len() => {
                     let process_name_to_terminate = &p[index - 1].0;
                     terminate_process(process_name_to_terminate);
+
+                    // Add a delay (e.g., 2 seconds) before reprinting the list
+                    sleep(Duration::from_secs(2));
                 }
                 _ => {
                     println!("Invalid index selected");
@@ -71,25 +75,4 @@ fn terminate_process(process_name: &str) {
     } else {
         println!("Failed to execute taskkill command");
     }
-}
-
-fn find_visible_windows() {
-    unsafe extern "system" fn enum_windows_callback(hwnd: HWND, _l_param: isize) -> winapi::ctypes::c_int {
-        let mut buffer = [0u16; 256];
-        GetWindowTextW(hwnd, buffer.as_mut_ptr(), buffer.len() as i32);
-    
-        let window_title = String::from_utf16_lossy(&buffer);
-    
-        if !window_title.trim().is_empty() && IsWindowVisible(hwnd) != 0 {
-            println!("Visible window: {}", window_title.trim());
-        }
-    
-        1 // Continue enumeration
-    }
-
-    unsafe {
-        EnumWindows(Some(enum_windows_callback), 0);
-    }
-
-    inputbot::handle_input_events();
 }
